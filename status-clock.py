bb1313a727fb368d582f815ec2f805ed258f8b8b
Elio Struyf <info@estruyf.be>
2020-04-27 19:55:46 +0200

    Added time

--- status-clock.py
+++ status-clock.py
@@ -37,16 +37,20 @@ draw = ImageDraw.Draw(img)
 meetingFont = ImageFont.truetype(os.path.join(PATH, "font/BetterPixels.ttf"), 16)
 # meetingTitle = meetingJson.get('title')
 meetingTitle = "Hey ... is it time for a chat??"
-titleXLoc = (inky_display.WIDTH / 2) - 20
+titleXLoc = (inky_display.WIDTH / 2) - 15
 titleYLoc = (inky_display.HEIGHT / 2) + 5
-titleLines = textwrap.wrap(meetingTitle, width = (212 - titleXLoc - 3))
+titleLines = textwrap.wrap(meetingTitle, width = 22)
 for line in titleLines:
     width, height = meetingFont.getsize(line)
     draw.text((titleXLoc, titleYLoc), line, inky_display.WHITE, meetingFont)
     titleYLoc += height
 
 # meetingTime = meetingJson.get('time')
-# timeX, timeY = timeFont.getsize(minutes)
+meetingTime = "tomorrow at 12:00 PM"
+timeWidth, timeHeight = meetingFont.getsize(meetingTime)
+timeXLoc = 212 - 10 - timeWidth
+timeYLoc = 96
+draw.text((timeXLoc, timeYLoc), meetingTime, inky_display.WHITE, meetingFont)
 
 # Write the time
 timeFont = ImageFont.truetype(FredokaOne, 45)
