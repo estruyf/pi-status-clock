422f015e318ade31324e3ba4a3ba23f7b4c42acd
Elio Struyf <info@estruyf.be>
2020-05-08 08:57:55 +0200

    Switch position

--- status-clock.py
+++ status-clock.py
@@ -131,14 +131,15 @@ if todoTasks is not None:
     todoFont = ImageFont.truetype(os.path.join(PATH, "font/BetterPixels.ttf"), 35)
     todoTxt = str(todoTasks)
     todoWidth, todoHeight = tempFont.getsize(todoTxt)
-    todoX = (inky_display.WIDTH / 2) - 10 - todoWidth
+    todoX = (inky_display.WIDTH - 36) - 10 - todoWidth
     todoY = 15
     draw.text((todoX, todoY), todoTxt, inky_display.BLACK, todoFont)
+    img.paste(icons["todo"], ((inky_display.WIDTH - 36) - 32 - 10 - todoWidth))
 
 # Write the availability
 availability = reqData.get('availability')
 if availability is not None:
-    img.paste(icons[availability], (212-36-5, 17))
+    img.paste(icons[availability], ((212/2)-36-5, 17))
 
 # Write the time
 timeFont = ImageFont.truetype(FredokaOne, 45)
