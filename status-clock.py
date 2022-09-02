f6eb30091877f0216d75ff84de22f9c4bf425755
Elio Struyf <elio@struyfconsulting.be>
2022-09-02 16:34:58 +0200

    Update font

--- status-clock.py
+++ status-clock.py
@@ -116,11 +116,11 @@ reqData = req.json()
 # Get the battery status
 batteryPercentage = reqData.get('battery')
 if batteryPercentage is not None:
-    batteryFont = ImageFont.truetype(os.path.join(PATH, "font/BetterPixels.ttf"), 35)
+    batteryFont = ImageFont.truetype(os.path.join(PATH, "font/BetterPixels.ttf"), 16)
     batteryText = str(batteryPercentage)
     batteryTextWidth, batteryTextHeight = batteryFont.getsize(batteryText)
     batteryX = inky_display.WIDTH - batteryTextWidth - 5
-    batteryY = 52 + 11
+    batteryY = 52 + 30
     draw.text((batteryX, batteryY), batteryText, inky_display.WHITE, batteryFont)
 
 # Write the temperature
